d29a4658fa32441ce3546a6a665b0ed95c2aa5ce
ao donovan <a.o.donovan.music@gmail.com>
2022-10-26 22:25:15 -0400

    new projects as well as tasks are now persistent between sessions

--- src/dombuilder.js
+++ src/dombuilder.js
@@ -1,48 +1,47 @@
-import {pageSelect} from "./index.js";
-import {removeTask} from "./index.js";
-export function domDisplay(taskList, currentProject){
-  let taskArea = document.getElementById("taskArea");
-  let list = [];
+import { pageSelect, removeTask } from './index.js';
+export function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+export function domDisplay(taskList, currentProject) {
+  const taskArea = document.getElementById('taskArea');
+  const list = [];
   removeAllChildNodes(taskArea);
-  for(let i = 0; i<taskList.length; i++){
-    if(taskList[i].project == currentProject){
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].project == currentProject) {
       list.push(taskList[i]);
     }
   }
-  function generateElements(task){
-    let wrap = document.createElement("div");
-    let label = document.createElement("label");
-    let htmlInput = document.createElement("input");
-    let span = document.createElement("span");
-    wrap.classList.add("form-control", "checklist");
-    label.classList.add("cursor-pointer", "taskLabel");
-    htmlInput.classList.add("checkbox", "checkbox-primary");
-    htmlInput.setAttribute("type", "checkbox");
-    htmlInput.setAttribute("data-label", task)
-    htmlInput.addEventListener("click", removeTask);
-    span.classList.add("label-text");
-    span.setAttribute("id", task);
+  function generateElements(task) {
+    const wrap = document.createElement('div');
+    const label = document.createElement('label');
+    const htmlInput = document.createElement('input');
+    const span = document.createElement('span');
+    wrap.classList.add('form-control', 'checklist');
+    label.classList.add('cursor-pointer', 'taskLabel');
+    htmlInput.classList.add('checkbox', 'checkbox-primary');
+    htmlInput.setAttribute('type', 'checkbox');
+    htmlInput.setAttribute('data-label', task);
+    htmlInput.addEventListener('click', removeTask);
+    span.classList.add('label-text');
+    span.setAttribute('id', task);
     label.appendChild(htmlInput);
     label.appendChild(span);
     wrap.appendChild(label);
     span.innerHTML = task;
     taskArea.appendChild(wrap);
-}
-  list.forEach(element => generateElements(element.taskName));
-}
-export function removeAllChildNodes(parent) {
-  while (parent.firstChild) {
-      parent.removeChild(parent.firstChild);
   }
+  list.forEach((element) => generateElements(element.taskName));
 }
-export function projManager(projName){
-  let projQ = document.getElementById("projQ");
-  let listItem = document.createElement("li");
-  let anchor = document.createElement("a");
-  anchor.classList.add("navBtn");
-  anchor.setAttribute("id", projName);
+export function projManager(projName) {
+  const projQ = document.getElementById('projQ');
+  const listItem = document.createElement('li');
+  const anchor = document.createElement('a');
+  anchor.classList.add('navBtn');
+  anchor.setAttribute('id', projName);
   anchor.innerHTML = projName;
-  anchor.addEventListener("click", pageSelect);
+  anchor.addEventListener('click', pageSelect);
   listItem.appendChild(anchor);
   projQ.appendChild(listItem);
   anchor.click();
--- src/index.js
+++ src/index.js
@@ -9,6 +9,17 @@ window.addEventListener('load', taskSet);
 navBtns.forEach((element) => element.addEventListener('click', pageSelect));
 newTaskBtn.addEventListener('click', taskCreate);
 newProjBtn.addEventListener('click', projCreate);
+const taskController = (() => {
+  const project = 'Today';
+  const taskList = [];
+  const taskRemove = (target) => taskController.taskList.splice(target, 1);
+  return {
+    project, taskList, taskRemove,
+};
+})();
+function storageManager() {
+  localStorage.setItem('saveState', JSON.stringify(taskController.taskList));
+}
 export function pageSelect() {
   const navBtns = document.querySelectorAll('.navBtn');
   navBtns.forEach((element) => element.classList.remove('active'));
@@ -16,11 +27,14 @@ export function pageSelect() {
   taskController.project = this.id;
   domDisplay(taskController.taskList, taskController.project);
 }
-const newTask = function (project, taskName) {
-  this.taskName = taskName;
+const NewTask = function (project, task) {
+  this.taskName = task;
   this.project = project;
 };
 function taskSet() {
+  function removeDuplicates(arr) {
+    return [...new Set(arr)];
+  }
   if (localStorage.length > 0) {
     taskController.taskList = JSON.parse(localStorage.getItem('saveState'));
     domDisplay(taskController.taskList, taskController.project);
@@ -28,20 +42,16 @@ function taskSet() {
     for (let i = 0; i < taskController.taskList.length; i++) {
       listT.push(taskController.taskList[i].project);
     }
-    function removeDuplicates(arr) {
-      return [...new Set(arr)];
-    }
     removeAllChildNodes(document.getElementById('projQ'));
     const newNav = removeDuplicates(listT);
     newNav.forEach((element) => projManager(element));
   }
 }
 function taskCreate() {
-  const taskObj = new newTask(taskController.project, taskName.value);
+  const taskObj = new NewTask(taskController.project, taskName.value);
   taskController.taskList.push(taskObj);
   taskName.value = '';
   domDisplay(taskController.taskList, taskController.project);
-  console.log(taskController.taskList);
   storageManager();
   taskSet();
 }
@@ -55,15 +65,4 @@ export function removeTask() {
   taskController.taskRemove(listIndex);
   domDisplay(taskController.taskList, taskController.project);
   storageManager();
-}
-const taskController = (() => {
-  const project = 'Today';
-  const taskList = [];
-  const taskRemove = (target) => taskController.taskList.splice(target, 1);
-  return {
-    project, taskList, taskRemove,
-  };
-})();
-function storageManager() {
-  localStorage.setItem('saveState', JSON.stringify(taskController.taskList));
 }
